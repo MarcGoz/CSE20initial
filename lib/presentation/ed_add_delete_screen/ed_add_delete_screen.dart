baa45d329843f7b7aebd2777b7f5ba7da0281112
SJunKwon <kwon.sungjun@auf.edu.ph>
2023-12-30 18:38:51 +0800

    Added Functionality to ed_add_delete_screen.dart

--- lib/presentation/ed_add_delete_screen/ed_add_delete_screen.dart
+++ lib/presentation/ed_add_delete_screen/ed_add_delete_screen.dart
@@ -8,11 +8,46 @@ import 'package:provider/provider.dart';
 import 'package:facetap/user_data.dart';
 import 'package:intl/intl.dart';
 import 'package:facetap/widgets/custom_elevated_button.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class EdAddDeleteScreen extends StatelessWidget {
+class Course {
+  late String className;
+  late String classCode;
+  late Map<String, Map<String, String>> weeklySchedule;
+  late DateTime schoolPeriodEnds;
+  late double latitude;
+  late double longitude;
+
+  Course({
+    required this.className,
+    required this.classCode,
+    required this.weeklySchedule,
+    required this.schoolPeriodEnds,
+    required this.latitude,
+    required this.longitude,
+  });
+}
+class EdAddDeleteScreen extends StatefulWidget {
   EdAddDeleteScreen({Key? key}) : super(key: key);
 
+  @override
+  EdAddDeleteScreenState createState() => EdAddDeleteScreenState();
+}
+
+
+
+class EdAddDeleteScreenState extends State<EdAddDeleteScreen> {
+
   GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+  final TextEditingController _classNameController = TextEditingController();
+  final TextEditingController _classCodeController = TextEditingController();
+  final TextEditingController _latitudeController = TextEditingController();
+  final TextEditingController _longitudeController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+  Map<String, TimeOfDay> _startTime = {};
+  Map<String, TimeOfDay> _endTime = {};
+  Map<String, Map<String, String>> _selectedDays = {};
+
 
   @override
   Widget build(BuildContext context) {
@@ -59,11 +94,11 @@ class EdAddDeleteScreen extends StatelessWidget {
               Expanded(
                 child: ListView(
                   children: [
-                    _buildFormField(context: context, label: 'Class Name', icon: Icons.class_),
-                    _buildFormField(context: context, label: 'Class Code', icon: Icons.confirmation_number),
+                    _buildFormField(context: context, label: 'Class Name', icon: Icons.class_, controller: _classNameController),
+                    _buildFormField(context: context, label: 'Class Code', icon: Icons.confirmation_number, controller: _classCodeController),
                     _buildCard(label: 'Weekly Schedule', icon: Icons.schedule, child: _buildWeeklyScheduleField(context)),
                     _buildDatePickerField(context),
-                    _buildFormField(context: context, label: 'Location', icon: Icons.location_on),
+                    _buildLocationFormField(context: context, label: 'Location', icon: Icons.location_on, latitudeController: _latitudeController, longitudeController: _longitudeController),
                   ],
                 ),
               ),
@@ -72,7 +107,17 @@ class EdAddDeleteScreen extends StatelessWidget {
                 height: 49,
                 text: "Add".toUpperCase(),
                 onPressed: () {
-                  print("Saved!");
+                  Course course = Course(
+                    className: _classNameController.text,
+                    classCode: _classCodeController.text,
+                    weeklySchedule: _selectedDays,
+                    schoolPeriodEnds: _selectedDate,
+                    latitude: double.parse(_latitudeController.text),
+                    longitude: double.parse(_longitudeController.text),
+                  );
+
+                  // Call a function to save the course to Firestore
+                  _saveCourseToFirestore(course);
                 },
               ),
             ],
@@ -83,7 +128,55 @@ class EdAddDeleteScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildFormField({required BuildContext context, required String label, required IconData icon}) {
+  Widget _buildLocationFormField({
+    required BuildContext context,
+    required IconData icon,
+    required String label,
+    required TextEditingController latitudeController,
+    required TextEditingController longitudeController,
+  }) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            controller: latitudeController,
+            decoration: InputDecoration(
+              labelText: 'Latitude',
+              icon: Icon(icon),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'This field is required';
+              }
+              return null;
+            },
+          ),
+        ),
+        const SizedBox(width: 10.0),
+        Expanded(
+          child: TextFormField(
+            controller: longitudeController,
+            decoration: InputDecoration(
+              labelText: 'Longitude',
+              icon: Icon(icon),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'This field is required';
+              }
+              return null;
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFormField({
+    required BuildContext context,
+    required String label,
+    required IconData icon,
+    required TextEditingController controller}) {
     if (label == 'Weekly Schedule') {
       // Exempt validation for Weekly Schedule Days
       return _buildCard(
@@ -91,43 +184,10 @@ class EdAddDeleteScreen extends StatelessWidget {
         icon: icon,
         child: _buildWeeklyScheduleField(context),
       );
-    } else if (label == 'Location') {
-      return Row(
-        children: [
-          Expanded(
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Latitude',
-                icon: Icon(icon),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'This field is required';
-                }
-                return null;
-              },
-            ),
-          ),
-          const SizedBox(width: 10.0),
-          Expanded(
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Longitude',
-                icon: Icon(icon),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'This field is required';
-                }
-                return null;
-              },
-            ),
-          ),
-        ],
-      );
     } else {
       // For other fields, make them required
       return TextFormField(
+        controller: controller,
         decoration: InputDecoration(
           labelText: label,
           icon: Icon(icon),
@@ -162,22 +222,54 @@ class EdAddDeleteScreen extends StatelessWidget {
   }
 
   Widget _buildWeeklyScheduleField(BuildContext context) {
+    List<String> days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
     return Column(
-      children: [
-        _buildDayTimerField(context: context, day: 'Monday'),
-        _buildDayTimerField(context: context, day: 'Tuesday'),
-        _buildDayTimerField(context: context, day: 'Wednesday'),
-        _buildDayTimerField(context: context, day: 'Thursday'),
-        _buildDayTimerField(context: context, day: 'Friday'),
-        _buildDayTimerField(context: context, day: 'Saturday'),
-        _buildDayTimerField(context: context, day: 'Sunday'),
-      ],
+      children: days.map((day) {
+        return _buildDayTimerField(
+          context: context,
+          day: day,
+          startTime: _startTime[day] ?? TimeOfDay.now(),
+          endTime: _endTime[day] ?? TimeOfDay.now(),
+          onStartTimeChanged: (newTime) {
+            setState(() {
+              _startTime[day] = newTime;
+              _updateSelectedDays(day);
+            });
+          },
+          onEndTimeChanged: (newTime) {
+            setState(() {
+              _endTime[day] = newTime;
+              _updateSelectedDays(day);
+            });
+          },
+        );
+      }).toList(),
     );
   }
+  void _updateSelectedDays(String day) {
+    String startTime = _startTime[day]?.format(context) ?? '';
+    String endTime = _endTime[day]?.format(context) ?? '';
 
-  Widget _buildDayTimerField({required BuildContext context, required String day}) {
-    TimeOfDay selectedTime = TimeOfDay.now();
+    if (startTime.isNotEmpty && endTime.isNotEmpty) {
+      setState(() {
+        _selectedDays[day] = {'start': startTime, 'end': endTime};
+      });
+    } else {
+      setState(() {
+        _selectedDays.remove(day);
+      });
+    }
+  }
 
+  Widget _buildDayTimerField({
+    required BuildContext context,
+    required String day,
+    required TimeOfDay startTime,
+    required TimeOfDay endTime,
+    required Function(TimeOfDay) onStartTimeChanged,
+    required Function(TimeOfDay) onEndTimeChanged,
+  }) {
     return Row(
       children: [
         Expanded(
@@ -192,21 +284,64 @@ class EdAddDeleteScreen extends StatelessWidget {
         const SizedBox(width: 10.0),
         Expanded(
           child: InkWell(
-            onTap: null,
+            onTap: () async {
+              TimeOfDay? pickedStartTime = await showTimePicker(
+                context: context,
+                initialTime: startTime,
+              );
+
+              if (pickedStartTime != null) {
+                // Call the callback function to update the state
+                onStartTimeChanged(pickedStartTime);
+              }
+            },
             child: Container(
               padding: const EdgeInsets.all(8.0),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.grey),
                 borderRadius: BorderRadius.circular(4.0),
               ),
-              child: const Row(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    'Select Time',
-                    style: TextStyle(fontSize: 16.0),
+                    '${startTime.format(context)}',
+                    style: const TextStyle(fontSize: 16.0),
                   ),
-                  Icon(Icons.access_time),
+                  const Icon(Icons.access_time),
+                ],
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 10.0),
+        Expanded(
+          child: InkWell(
+            onTap: () async {
+              TimeOfDay? pickedEndTime = await showTimePicker(
+                context: context,
+                initialTime: endTime,
+              );
+
+              if (pickedEndTime != null) {
+                // Call the callback function to update the state
+                onEndTimeChanged(pickedEndTime);
+              }
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '${endTime.format(context)}',
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                  const Icon(Icons.access_time),
                 ],
               ),
             ),
@@ -217,8 +352,6 @@ class EdAddDeleteScreen extends StatelessWidget {
   }
 
   Widget _buildDatePickerField(BuildContext context) {
-    DateTime selectedDate = DateTime.now();
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -230,7 +363,7 @@ class EdAddDeleteScreen extends StatelessWidget {
           onTap: () async {
             DateTime? pickedDate = await showDatePicker(
               context: context,
-              initialDate: selectedDate,
+              initialDate: _selectedDate,
               firstDate: DateTime(2000),
               lastDate: DateTime(2101),
               builder: (BuildContext context, Widget? child) {
@@ -245,8 +378,10 @@ class EdAddDeleteScreen extends StatelessWidget {
               },
             );
 
-            if (pickedDate != null && pickedDate != selectedDate) {
-              selectedDate = pickedDate;
+            if (pickedDate != null && pickedDate != _selectedDate) {
+              setState(() {
+                _selectedDate = pickedDate;
+              });
             }
           },
           child: Container(
@@ -259,7 +394,7 @@ class EdAddDeleteScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  DateFormat('yyyy-MM-dd').format(selectedDate),
+                  DateFormat('yyyy-MM-dd').format(_selectedDate),
                   style: const TextStyle(fontSize: 16.0),
                 ),
                 const Icon(Icons.calendar_today),
@@ -347,5 +482,30 @@ class EdAddDeleteScreen extends StatelessWidget {
         return BottomBarEnum.Attendance;
       },
     );
+  }
+  Future<void> _saveCourseToFirestore(Course course) async {
+    try {
+      // Get a reference to the 'Course' collection
+      CollectionReference courseCollection = FirebaseFirestore.instance.collection('Courses');
+
+      // Add the course data to Firestore
+      await courseCollection.add({
+        'className': course.className,
+        'classCode': course.classCode,
+        'weeklySchedule': course.weeklySchedule,
+        'schoolPeriodEnds': course.schoolPeriodEnds,
+        'latitude': course.latitude,
+        'longitude': course.longitude,
+      });
+
+      // Show a success message or navigate to another screen if needed
+      print('Course added to Firestore successfully!');
+
+      // Navigate back to the dashboard
+      Navigator.of(context).pushReplacementNamed(AppRoutes.teacherDashboardHomeScreen);
+    } catch (e) {
+      // Handle any errors that occur during the process
+      print('Error adding course to Firestore: $e');
+    }
   }
 }
